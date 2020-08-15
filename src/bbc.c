87c3877b88d4fc452d58f2c6c00b6575ade2e68d
Maksim Korzh <freesoft.for.people@gmail.com>
2020-08-15 14:16:29 +0300

    added parse FEN function

---
+++ src/bbc.c
@@ -0,0 +1,539 @@
+/**********************************\
+ ==================================
+  
+                BBC
+                
+ ==================================
+          
+       bitboard chess engine
+       
+                by
+                
+         Code Monkey King
+ 
+ ==================================
+\**********************************/
+
+// include system headers
+#include <stdio.h>
+#include <string.h>
+#ifdef WIN64
+#include "windows.h"
+#else
+#include "sys/time.h"
+#include "sys/select.h"
+#include "string.h"
+#endif
+
+// FEN dedug positions
+#define start_position "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 "
+#define tricky_position "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 "
+#define killer_position "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
+#define cmk_position "r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9 "
+
+// square encoding
+enum {
+    a8, b8, c8, d8, e8, f8, g8, h8,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a1, b1, c1, d1, e1, f1, g1, h1, no_sq
+};
+
+// piece encoding
+enum { P, N, B, R, Q, K, p, n, b, r, q, k };
+
+// convert board square indexes to coordinates
+char *square_to_coords[] = {
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+};
+
+// capture flags
+enum capture_flags {all_moves, only_captures};
+
+// castling binary representation
+//
+//  bin  dec
+// 0001    1  white king can castle to the king side
+// 0010    2  white king can castle to the queen side
+// 0100    4  black king can castle to the king side
+// 1000    8  black king can castle to the queen side
+//
+// examples
+// 1111       both sides an castle both directions
+// 1001       black king => queen side
+//            white king => king side
+
+// castling writes
+enum castling { KC = 1, QC = 2, kc = 4, qc = 8 };
+
+// sides to move
+enum sides { white, black };
+
+// ascii pieces
+char ascii_pieces[] = "PNBRQKpnbrqk";
+
+// unicode pieces
+char *unicode_pieces[] = {"♙", "♘", "♗", "♖", "♕", "♔", "♟︎", "♞", "♝", "♜", "♛", "♚"};
+
+// encode ascii pieces
+int char_pieces[] = {
+    ['P'] = P,
+    ['N'] = N,
+    ['B'] = B,
+    ['R'] = R,
+    ['Q'] = Q,
+    ['K'] = K,
+    ['p'] = p,
+    ['n'] = n,
+    ['b'] = b,
+    ['r'] = r,
+    ['q'] = q,
+    ['k'] = k,
+};
+
+// decode promoted pieces
+int promoted_pieces[] = {
+    [Q] = 'q',
+    [R] = 'r',
+    [B] = 'b',
+    [N] = 'n',
+    [q] = 'q',
+    [r] = 'r',
+    [b] = 'b',
+    [n] = 'n',
+};
+
+// material scrore
+
+/*
+    ♙ =   100   = ♙
+    ♘ =   300   = ♙ * 3
+    ♗ =   350   = ♙ * 3 + ♙ * 0.5
+    ♖ =   500   = ♙ * 5
+    ♕ =   1000  = ♙ * 10
+    ♔ =   10000 = ♙ * 100
+    
+*/
+
+int material_score[13] = {
+      0,      // empty square score
+    100,      // white pawn score
+    300,      // white knight scrore
+    350,      // white bishop score
+    500,      // white rook score
+   1000,      // white queen score
+  10000,      // white king score
+   -100,      // black pawn score
+   -300,      // black knight scrore
+   -350,      // black bishop score
+   -500,      // black rook score
+  -1000,      // black queen score
+ -10000,      // black king score
+    
+};
+
+// castling rights
+
+/*
+                             castle   move     in      in
+                              right    map     binary  decimal
+
+        white king  moved:     1111 & 1100  =  1100    12
+  white king's rook moved:     1111 & 1110  =  1110    14
+ white queen's rook moved:     1111 & 1101  =  1101    13
+     
+         black king moved:     1111 & 0011  =  1011    3
+  black king's rook moved:     1111 & 1011  =  1011    11
+ black queen's rook moved:     1111 & 0111  =  0111    7
+
+*/
+
+int castling_rights[128] = {
+     7, 15, 15, 15,  3, 15, 15, 11,
+    15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15,
+    13, 15, 15, 15, 12, 15, 15, 14
+};
+
+// pawn positional score
+const int pawn_score[128] = 
+{
+    90,  90,  90,  90,  90,  90,  90,  90,
+    30,  30,  30,  40,  40,  30,  30,  30,
+    20,  20,  20,  30,  30,  30,  20,  20,
+    10,  10,  10,  20,  20,  10,  10,  10,
+     5,   5,  10,  20,  20,   5,   5,   5,
+     0,   0,   0,   5,   5,   0,   0,   0,
+     0,   0,   0, -10, -10,   0,   0,   0,
+     0,   0,   0,   0,   0,   0,   0,   0
+};
+
+// knight positional score
+const int knight_score[128] = 
+{
+    -5,   0,   0,   0,   0,   0,   0,  -5,
+    -5,   0,   0,  10,  10,   0,   0,  -5,
+    -5,   5,  20,  20,  20,  20,   5,  -5,
+    -5,  10,  20,  30,  30,  20,  10,  -5,
+    -5,  10,  20,  30,  30,  20,  10,  -5,
+    -5,   5,  20,  10,  10,  20,   5,  -5,
+    -5,   0,   0,   0,   0,   0,   0,  -5,
+    -5, -10,   0,   0,   0,   0, -10,  -5
+};
+
+// bishop positional score
+const int bishop_score[128] = 
+{
+     0,   0,   0,   0,   0,   0,   0,   0,
+     0,   0,   0,   0,   0,   0,   0,   0,
+     0,   0,   0,  10,  10,   0,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,  10,   0,   0,   0,   0,  10,   0,
+     0,  30,   0,   0,   0,   0,  30,   0,
+     0,   0, -10,   0,   0, -10,   0,   0
+
+};
+
+// rook positional score
+const int rook_score[128] =
+{
+    50,  50,  50,  50,  50,  50,  50,  50,
+    50,  50,  50,  50,  50,  50,  50,  50,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,  10,  20,  20,  10,   0,   0,
+     0,   0,   0,  20,  20,   0,   0,   0
+
+};
+
+// king positional score
+const int king_score[128] = 
+{
+     0,   0,   0,   0,   0,   0,   0,   0,
+     0,   0,   5,   5,   5,   5,   0,   0,
+     0,   5,   5,  10,  10,   5,   5,   0,
+     0,   5,  10,  20,  20,  10,   5,   0,
+     0,   5,  10,  20,  20,  10,   5,   0,
+     0,   0,   5,  10,  10,   5,   0,   0,
+     0,   5,   5,  -5,  -5,   0,   5,   0,
+     0,   0,   5,   0, -15,   0,  10,   0
+
+};
+
+// mirror positional score tables for opposite side
+const int mirror_score[128] =
+{
+	a1, b1, c1, d1, e1, f1, g1, h1,
+	a2, b2, c2, d2, e2, f2, g2, h2,
+	a3, b3, c3, d3, e3, f3, g3, h3,
+	a4, b4, c4, d4, e4, f4, g4, h4,
+	a5, b5, c5, d5, e5, f5, g5, h5,
+	a6, b6, c6, d6, e6, f6, g6, h6,
+	a7, b7, c7, d7, e7, f7, g7, h7,
+	a8, b8, c8, d8, e8, f8, g8, h8
+};
+
+/*
+    Move formatting
+    
+    0000 0000 0000 0000 0111 1111       source square
+    0000 0000 0011 1111 1000 0000       target square
+    0000 0011 1100 0000 0000 0000       promoted piece
+    0000 0100 0000 0000 0000 0000       capture flag
+    0000 1000 0000 0000 0000 0000       double pawn flag
+    0001 0000 0000 0000 0000 0000       enpassant flag
+    0010 0000 0000 0000 0000 0000       castling
+
+*/
+
+// encode move
+#define encode_move(source, target, piece, capture, pawn, enpassant, castling) \
+(                          \
+    (source) |             \
+    (target << 7) |        \
+    (piece << 14) |        \
+    (capture << 18) |      \
+    (pawn << 19) |         \
+    (enpassant << 20) |    \
+    (castling << 21)       \
+)
+
+// decode move's source square
+#define get_move_source(move) (move & 0x7f)
+
+// decode move's target square
+#define get_move_target(move) ((move >> 7) & 0x7f)
+
+// decode move's promoted piece
+#define get_move_piece(move) ((move >> 14) & 0xf)
+
+// decode move's capture flag
+#define get_move_capture(move) ((move >> 18) & 0x1)
+
+// decode move's double pawn push flag
+#define get_move_pawn(move) ((move >> 19) & 0x1)
+
+// decode move's enpassant flag
+#define get_move_enpassant(move) ((move >> 20) & 0x1)
+
+// decode move's castling flag
+#define get_move_castling(move) ((move >> 21) & 0x1)
+
+// define bitboard data type
+#define U64 unsigned long long
+
+// manipulate bits
+#define get_bit(bitboard, square) (bitboard & (1ULL << square))
+#define set_bit(bitboard, square) (bitboard |= (1ULL << square))
+#define pop_bit(bitboard, square) (get_bit(bitboard, square) ? (bitboard ^= (1ULL << square)) : 0)
+
+// bitboards for all pieces (empty square & 12 pieces)
+U64 bitboards[13];
+
+// occupancy bitboards (all white & black pieces)
+U64 occupancy[2];
+
+// side to move | enpassant square | castling right
+int side, enpassant, castle;
+
+// kings' squares
+int king_square[2];
+
+
+/**********************************\
+ ==================================
+ 
+           Input & Output
+ 
+ ==================================
+\**********************************/
+
+// print bitboard
+void print_bitboard(U64 bitboard)
+{
+    printf("\n");
+    
+    // loop over board ranks
+    for (int rank = 0; rank < 8; rank++)
+    {
+        // loop over board files
+        for (int file = 0; file < 8; file++)
+        {
+            // init board square
+            int square = rank * 8 + file;
+            
+            // print ranks
+            if (!file)
+                printf("  %d ", 8 - rank);
+            
+            // print bit indexed by board square
+            printf(" %d", get_bit(bitboard, square) ? 1 : 0);
+        }
+        
+        printf("\n");
+    }
+    
+    printf("\n     a b c d e f g h\n\n");
+}
+
+// print chess board
+void print_board()
+{
+    printf("\n");
+    
+    // loop over board ranks
+    for (int rank = 0; rank < 8; rank++)
+    {
+        // loop over board files
+        for (int file = 0; file < 8; file++)
+        {
+            // init board square
+            int square = rank * 8 + file;
+            
+            // print ranks
+            if (!file)
+                printf("  %d ", 8 - rank);
+            
+            // init piece
+            int piece = -1;
+            
+            // loop over bitboards
+            for (int bb_piece = P; bb_piece <= k; bb_piece++)
+            {
+                // if piece is on bitboard
+                if (get_bit(bitboards[bb_piece], square))
+                {
+                    // init piece
+                    piece = bb_piece;
+                    break;
+                }
+            }
+            
+            #ifdef WIN64
+                // print ASCII pieces on windows
+                printf(" %c", (piece == -1) ? '.' : ascii_pieces[piece]);
+            #else
+                // print UNICODE pieces on linux
+                printf(" %s", (piece == -1) ? "." : unicode_pieces[piece]);
+            #endif
+        }
+        
+        printf("\n");
+    }
+    
+    // print files
+    printf("\n     a b c d e f g h\n\n");
+    
+    // print board stats
+    printf("    Side:     %s\n", (side == white) ? "white": "black");
+    printf("    Castling:  %c%c%c%c\n", (castle & KC) ? 'K' : '-', 
+                                        (castle & QC) ? 'Q' : '-',
+                                        (castle & kc) ? 'k' : '-',
+                                        (castle & qc) ? 'q' : '-');
+    printf("    Enpassant:   %s\n", (enpassant == no_sq)? "no" : square_to_coords[enpassant]);
+    printf("    King square: %s\n\n", square_to_coords[king_square[side]]);
+}
+
+// parse FEN
+void parse_fen(char *fen)
+{
+    // reset bitboards
+    memset(bitboards, 0ULL, sizeof(bitboards));
+    
+    // loop over board ranks
+    for (int rank = 0; rank < 8; rank++)
+    {
+        // loop over board files
+        for (int file = 0; file < 8; file++)
+        {
+            // init square
+            int square = rank * 8 + file;
+        
+            // match pieces
+            if ((*fen >= 'a' && *fen <= 'z') || (*fen >= 'A' && *fen <= 'Z'))
+            {
+                // set up kings' squares
+                if (*fen == 'K')
+                    king_square[white] = square;
+                
+                else if (*fen == 'k')
+                    king_square[black] = square;
+                
+                // init piece type
+                int piece = char_pieces[*fen];
+
+                // set the piece on board
+                set_bit(bitboards[piece], square);
+                
+                // increment FEN pointer
+                *fen++;
+            }
+            
+            // match empty squares
+            if (*fen >= '0' && *fen <= '9')
+            {
+                // calculate offset
+                int offset = *fen - '0';
+                
+                // init piece
+                int piece = -1;
+                
+                // loop over bitboards
+                for (int bb_piece = P; bb_piece <= k; bb_piece++)
+                {
+                    // if piece is on bitboard
+                    if (get_bit(bitboards[bb_piece], square))
+                    {
+                        // init piece
+                        piece = bb_piece;
+                        break;
+                    }
+                }
+                
+                // if no piece on current square
+                if (piece == -1)
+                    // decrement file
+                    file--;
+                
+                // skip empty squares
+                file += offset;
+                
+                // increment FEN pointer
+                *fen++;
+            }
+            
+            // match end of rank
+            if (*fen == '/')
+                // increment FEN pointer
+                *fen++;
+        }
+    }
+    
+    // go to side parsing
+    *fen++;
+    
+    // parse side to move
+    side = (*fen == 'w') ? white : black;
+    
+    // go to castling rights parsing
+    fen += 2;
+    
+    // parse castling rights
+    while (*fen != ' ')
+    {
+        switch(*fen)
+        {
+            case 'K': castle |= KC; break;
+            case 'Q': castle |= QC; break;
+            case 'k': castle |= kc; break;
+            case 'q': castle |= qc; break;
+            case '-': break;
+        }
+        
+        // increment pointer
+        *fen++;
+    }
+    
+    // got to empassant square
+    *fen++;
+    
+    // parse empassant square
+    if (*fen != '-')
+    {
+        // parse enpassant square's file & rank
+        int file = fen[0] - 'a';
+        int rank = 8 - (fen[1] - '0');
+        
+        // set up enpassant square
+        enpassant = rank * 8 + file;
+    }
+    
+    else
+        enpassant = no_sq;   
+}
+
+int main()
+{
+    parse_fen(tricky_position);
+    print_board();
+     
+    return 0;
+}

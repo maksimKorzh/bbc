ed7bccaf20c30b34235ebcdc16ef63a5cff355af
Maksim Korzh <freesoft.for.people@gmail.com>
2020-09-24 22:22:28 +0300

    added licence, updated readme, added tutorial files

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
-# bbc
-Bit Board Chess (BBC) - Didactic bitboard chess engine by Code Monkey King
+# BBC (Bit Board Chess)
+UCI chess engine by Code Monkey King<br>
+written for didactic purposes and<br>
+covered in 84 YouTube video series
 
 # YouTube tutorials
 [![IMAGE ALT TEXT HERE](https://img.youtube.com/vi/QUNP-UjujBM/0.jpg)](https://www.youtube.com/watch?v=QUNP-UjujBM&list=PLmN0neTso3Jxh8ZIylk74JpwfiWNI76Cs)
 
+# Features
+ - bitboard board representation
+ - pre-calculated attack tables
+ - magic bitboards for sliding pieces
+ - encoding moves as integers
+ - copy/make approach for making moves
+ - negamax search with alpha beta pruning
+ - PV/killer/history move ordering
+ - iterative deepening
+ - PVS (Principle Variation Search)
+ - LMR (Late Move Reduction)
+ - NMP (Null Move Pruning)
+ - Transposition table (20MB fixed size)
+ - Material/PST (Positional Square Tables) evaluation
+ - Double/isolated/passed pawns evaluation
+ - Bishop & Queen mobility evaluation
+ - Very basic king safety
+ - UCI protocol
+
+# Credits
+  
+  This project would never be completed<br>
+  without the help of Talk Chess forum<br>
+  community members who has been helping me<br>
+  during the development process significantly<br>
+  I would like to thank to:<br>
+  <br>
+  HaraldLuessen & Pedro Castro Elgarresta for
+  neverending hints and technical support
+  <br>
+  Jay Warendorff for testing development versions of BBC<br>
+  <br>
+  And all the subscribers watching my videos and<br>
+  motivating me to keep the development!<br>
+
+# Support the project
+  (https://www.patreon.com/code_monkey_king)
+  
+  
+  
+  
+  
--- src/bbc.c
+++ src/bbc.c
@@ -7,9 +7,79 @@
                 by
                 
          Code Monkey King
- 
+
  ==================================
 \**********************************/
+
+/**********************************\
+ ==================================
+
+      THIS ENGINE IS DEDICATED
+    TO HOBBY PROGRAMMERS FACING
+      ISSUES WITH READING CODE
+    OF OPEN SOURCE CHESS ENGINES
+  --------------------------------
+   This engine is incredibly easy
+  to read and understand. It uses
+   global variables everywhere it
+   is possible and sometimes even
+ sacrifices performance for clarity
+
+ ==================================
+\**********************************/
+
+/**********************************\
+ ==================================
+  
+   THIS PROGRAM IS FREE SOFTWARE.
+   IT COMES WITHOUT ANY WARRANTY,
+    TO THE EXTENT PERMITTED BY
+          APPLICABLE LAW.
+         
+   YOU CAN REDISTRIBUTE IT AND/OR
+    MODIFY IT UNDER THE TERMS OF
+  THE DO WHAT THE FUCK YOU WANT TO
+      PUBLIC LICENCE VERSION 2
+     AS PUBLISHED BY SAM HOCEVAR
+      SEE http://www.wtfpl.net/
+          FOR MORE DETAILS
+          
+  --------------------------------
+    Copyright © 2020 Maksim Korzh
+   <freesoft.for.people@gmail.com>
+   
+ ==================================
+\**********************************/
+
+/**********************************\
+ ==================================
+  
+     DO WHAT THE FUCK YOU WANT
+        TO PUBLIC LICENSE
+  --------------------------------
+     Version 2, December 2004
+  --------------------------------
+    Everyone is permitted to copy
+      and distribute verbatim or
+   modified copies of this license
+    document, and changing it is
+         allowed as long as
+        the name is changed
+  --------------------------------
+      DO WHAT THE FUCK YOU WANT
+         TO PUBLIC LICENSE
+  TERMS AND CONDITIONS FOR COPYING,
+    DISTRIBUTION AND MODIFICATION
+  
+   0. You just DO WHAT THE FUCK
+                 YOU WANT TO.
+  --------------------------------
+   Copyright (C) 2004 Sam Hocevar
+        <sam@hocevar.net>
+   
+ ==================================
+\**********************************/
+
 
 // system headers
 #include <stdio.h>
@@ -3201,8 +3271,8 @@ int follow_pv, score_pv;
  ==================================
 \**********************************/
 
-// hash table size
-#define hash_size 0x400000
+// hash table size (would be around 20MB)
+#define hash_size 800000
 
 // no hash entry found constant
 #define no_hash_entry 100000
